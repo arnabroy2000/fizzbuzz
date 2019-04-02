6516ec900e5cbadc26a74026dfed76d4cc7cf3d2
arnabroy2000 <49182658+arnabroy2000@users.noreply.github.com>
2019-04-02 20:15:29 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for i in range(1,51)
+  if(i%3==0 and i%5==0):
+   print(str(i)+"=fizzbuzz")
+  else:
+   if(i%3==0):
+    print(str(i)+"=fizz")
+  else:
+   if(i%5==0):
+    print(str(i)+"=buzz")
+      else:
+          print(str(i))
